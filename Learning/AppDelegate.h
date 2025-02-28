32193f1931f1be95b185b3db12418d329793f5f5
HuyLe-11 <huylekq11@gmail.com>
2025-02-28 12:50:16 +0700

    Initial Commit

---
+++ Learning/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Learning
+//
+//  Created by Huy Le's Macbook on 28/2/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Learning/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Learning
+//
+//  Created by Huy Le's Macbook on 28/2/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
